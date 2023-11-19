f547cf0cc49d41c74ff837a2f7363bb4454efc19
Annzel <zhongningze@126.com>
2023-11-19 22:54:28 +0800

    evaluate update

---
+++ evaluate_ddpg.py
@@ -0,0 +1,56 @@
+from utils.load_model import load_ddpg
+from algorithm.DDPG import DDPG
+from multiagent.mpe.predator_prey import predator_prey
+from multiagent.mpe._mpe_utils.simple_env import SimpleEnv
+import numpy as np
+
+save_dir = 'DDPG_DDPG_models'
+
+env = predator_prey.parallel_env(render_mode="rgb_array", max_cycles=25)
+observations, infos = env.reset()
+
+ddpg_agent_predator_0 = DDPG(obs_dim=env.observation_space("predator_0").shape[0], act_dim=env.action_space("predator_0").n, hidden_size=128, seed=10)
+ddpg_agent_predator_1 = DDPG(obs_dim=env.observation_space("predator_1").shape[0], act_dim=env.action_space("predator_1").n, hidden_size=128, seed=20)
+ddpg_agent_predator_2 = DDPG(obs_dim=env.observation_space("predator_2").shape[0], act_dim=env.action_space("predator_2").n, hidden_size=128, seed=30)
+ddpg_agent_prey_0 = DDPG(obs_dim=env.observation_space("prey_0").shape[0], act_dim=env.action_space("prey_0").n, hidden_size=128, seed=40)
+
+# Load the models for each agent
+load_ddpg(ddpg_agent_predator_0, 'ddpg_agent_predator_0', save_dir)
+load_ddpg(ddpg_agent_predator_1, 'ddpg_agent_predator_1', save_dir)
+load_ddpg(ddpg_agent_predator_2, 'ddpg_agent_predator_2', save_dir)
+load_ddpg(ddpg_agent_prey_0, 'ddpg_agent_prey_0', save_dir)
+
+def evaluate_model(num_episodes=10):
+    total_rewards = []
+
+    for episode in range(num_episodes):
+        episode_rewards = []
+        frames = []
+        observations, _ = env.reset()
+        while env.agents:
+            actions = {}
+            for agent, obs in observations.items():
+                if "predator_0" in agent:
+                    actions[agent] = ddpg_agent_predator_0.act(obs)
+                elif "predator_1" in agent:
+                    actions[agent] = ddpg_agent_predator_1.act(obs)
+                elif "predator_2" in agent:
+                    actions[agent] = ddpg_agent_predator_2.act(obs)
+                else:
+                    actions[agent] = ddpg_agent_prey_0.act(obs)
+
+            # Take the chosen actions and observe the next state and rewards
+            next_observations, rewards, infos, done, _ = env.step(actions)
+            frames.append(env.render())
+
+            episode_rewards.append(sum(rewards.values()))
+            observations = next_observations
+        SimpleEnv.display_frames_as_gif(frames,episode)
+
+        total_rewards.append(episode_rewards)
+
+    avg_reward = np.mean(total_rewards)
+    print(f'Average Reward over {num_episodes} episodes: {avg_reward}')
+
+evaluate_model(num_episodes=10)
+env.close()
